e5b3d978036cdbdc65caaa7cfdbb03a2c6058ecb
Xiaojun YU <xyuae@connect.ust.hk>
2017-09-07 14:23:08 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Router, browserHistory} from 'react-router'
+import Routes from './routes'
+
+ReactDOM.render(
+  <Router
+    history={browserHistory}
+    routes={Routes}
+  />,
+  document.getElementById('root')
+) //ReactDOM.render
